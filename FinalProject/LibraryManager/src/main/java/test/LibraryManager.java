5dfc65a182d34da2fd4dbfe04d9603209975c18f
adkm2201 <118100878+adkm2201@users.noreply.github.com>
2025-03-11 23:26:21 +0700

    red is dangerous..

---
+++ FinalProject/LibraryManager/src/main/java/test/LibraryManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test;
+
+/**
+ *
+ * @author adkm2
+ */
+public class LibraryManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
